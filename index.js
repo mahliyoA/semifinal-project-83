dc57c2a227a738a719b08763f9f0414c12366680
Makhliyo <azimovamahliyo>
2025-11-13 16:10:13 +0500

    tayyorlanyapdi

---
+++ index.js
@@ -0,0 +1,14 @@
+import TelegramBot from "node-telegram-bot-api";
+const TOKEN = "8117923609:AAHQIeFNaoBCw9cJGlkxh51y3xVLJkMbOZA"
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+
+
+
+
+
+
+
+
+console.log("bot ishga tushdi");
